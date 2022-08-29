00fad572af275ce0903cde8c981b47affda2d6e8
MehmetKLl <110687128+MehmetKLl@users.noreply.github.com>
2022-08-29 14:10:22 +0300

    Update env_paths.py

--- src/utils/env_paths.py
+++ src/utils/env_paths.py
@@ -1,6 +1,6 @@
 from os import environ
 
-PROGRAM_PATH = f"{environ['APPDATA'] if 'APPDATA' in environ.keys() else environ['PROGRAMFILES']}\\OBA Otomatik Oynatici"
+PROGRAM_PATH = f"{environ['APPDATA']}\\OBA Otomatik Oynatici"
 SETUP_PATH = f"{environ['TEMP']}\\OBA Setup"
 TEMP_PATH = f"{environ['TEMP']}\\Oba Otomatik Oynatici"
 LOG_PATH = f"{environ['TEMP']}\\OBA Log"
