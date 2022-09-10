2baed12defd3e5a7496dceea51c0cf49ce7cafa3
MehmetKLl <110687128+MehmetKLl@users.noreply.github.com>
2022-09-10 11:58:54 +0300

    Update main.py

--- src/utils/main.py
+++ src/utils/main.py
@@ -2,7 +2,6 @@ from pyautogui import locateOnScreen, FAILSAFE, click, moveTo, scroll, locateAll
 from time import sleep
 from utils.log import Log
 
-log = Log()
 
 def scroll_new_video():
     x, y, w, h = locateOnScreen("img/border.png")
@@ -58,6 +57,7 @@ def control_current_video():
 start = scroll_new_video
 
 def main():
+    log = Log() 
     try:
         start()
     except Exception as err:
