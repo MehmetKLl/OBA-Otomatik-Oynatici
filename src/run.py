eb94151819fced5d6a1f2453df35e22b024e384b
MehmetKLl <110687128+MehmetKLl@users.noreply.github.com>
2022-10-08 16:23:34 +0300

    Update run.py

--- src/run.py
+++ src/run.py
@@ -2,182 +2,178 @@ from traceback import format_exc
 from sys import exit
 from requests import Session, exceptions
 from utils.process import kill, start, process_list
-from utils.env_paths import PROGRAM_PATH, TEMP_PATH, SETUP_PATH
+from utils.env_paths import PROGRAM_PATH, TEMP_PATH, FULL_KEY_PATH, KEY_PATH, KEY_NAME
 from utils.log import Log
 from runtools import registry, server, file, gui
 from winreg import HKEY_CURRENT_USER
+from time import sleep
 
-VERIFY_SSL = True
+verify_ssl = True
+log = None  
 
-def start_installer():
-    global VERIFY_SSL
-    install_log = Log("Installer")
-    
-    install_log.write("Getting app contents from Github...")
+def install_program_contents(content_bytes,mode):
     try:
-        content_bytes = server.get_program_contents(timeout=3,verify=VERIFY_SSL)
-    except exceptions.SSLError:
-        install_log.write(f"An error occured due to SSL certificate authentication:\n{format_exc()}\n")
-        
-        ask_no_ssl_verify = gui.ask_and_show_error("SSL doğrulaması başarısız oldu. SSL doğrulamasını es geçip gene de devam etmek istiyor musunuz?\n(Bu yöntem güvenlik açıklarına sebep olacağından tavsiye edilmez.)","Player | Kurulum")        
-        if ask_no_ssl_verify:
-            VERIFY_SSL = False
-            start_installer()
-        else:
-            exit(1)
+        log.write(f"Creating temporary folder: \"{TEMP_PATH}\"")
+        file.create_dir(TEMP_PATH)
+
+        log.write(f"Downloading app contents...")
+        file.write_byte(f"{TEMP_PATH}\\executable.zip",content_bytes)
+        log.write(f"App contents have downloaded.")
+
+        if mode == "update":
+            log.write("App folder is cleaning...")
+            file.remove_all(PROGRAM_PATH)
+            file.create_dir(PROGRAM_PATH)
+            log.write("App folder been cleaned.")
+
+        elif mode == "install":
+            log.write(f"Creating app folder: \"{PROGRAM_PATH}\"")
+            file.create_dir(PROGRAM_PATH)
+            log.write(f"App folder created: \"{PROGRAM_PATH}\"")
+
+        log.write("Extracting contents from temporary zip file...")
+        file.extract(f"{TEMP_PATH}\\executable.zip",PROGRAM_PATH)
+        log.write("Contents have extracted from zip file.")
+
+        log.write("Deleting temporary setup folder...")
+        file.remove_all(TEMP_PATH)
+        log.write("Temporary setup folder been deleted.")
+                
+        if mode == "update":
+            log.write(f"Updating registry value at: \"{FULL_KEY_PATH}\"")
+            registry.create_key(HKEY_CURRENT_USER,KEY_PATH,KEY_NAME,server.get_version())
+            log.write(f"Updated registry value at: \"{FULL_KEY_PATH}\"")
+
+        elif mode == "install":
+            log.write(f"Creating registry value at: \"{FULL_KEY_PATH}\"")
+            registry.create_key(HKEY_CURRENT_USER,KEY_PATH,KEY_NAME,server.get_version())
+            log.write(f"Created registry value at: \"{FULL_KEY_PATH}\"")
             
-    except exceptions.ConnectTimeout:
-        install_log.write(f"Connection timed out:\n{format_exc()}\n")
-        
-        gui.show_error("Sunucuya gönderilen istek zaman aşımına uğradı.","Player | Kurulum")
-        exit(1)
-        
-    except exceptions.ConnectionError:
-        install_log.write(f"An error occured while getting app contents from Github:\n{format_exc()}\n")
-        
-        gui.show_error("Program internet olmadığından bilgisayara indirilemedi.","Player | Kurulum")
-        exit(1)
-
-    try:
-        install_log.write(f"Creating temporary folder: \"{SETUP_PATH}\"")
-        file.create_dir(SETUP_PATH)
-
-        install_log.write(f"Downloading app contents...")
-        file.write_byte(f"{SETUP_PATH}\\executable.zip",content_bytes)
-        install_log.write(f"App contents have downloaded.")
-
-        install_log.write(f"Creating contents' folder: \"{PROGRAM_PATH}\"")
-        file.create_dir(PROGRAM_PATH)
-        install_log.write(f"Contents' folder created: \"{PROGRAM_PATH}\"")
-
-        install_log.write("Extracting contents from temporary zip file...")
-        file.extract(f"{SETUP_PATH}\\executable.zip",PROGRAM_PATH)
-        install_log.write("Contents have extracted from zip file.")
-
-        install_log.write("Deleting temporary setup folder...")
-        file.remove_all(SETUP_PATH)
-        install_log.write("Temporary setup folder been deleted.")
-            
-        install_log.write("Creating registry value at: \"HKEY_CURRENT_USER\\SOFTWARE\\Player\\version\"")
-        registry.create_key(HKEY_CURRENT_USER,"SOFTWARE\\Player","version",server.get_version())
-        install_log.write("Created registry value at: \"HKEY_CURRENT_USER\\SOFTWARE\\Player\\version\"")
-
     except PermissionError as exc:
-        install_log.write(f"An error occured due to system permissions:\n{format_exc()}\n")
-        gui.show_error("Yetki hatası. Programı yönetici olarak çalıştırmayı deneyin.","Player | Kurulum")
-        return 1
+        gui.show_error("Yetki hatası. Uygulamayı yönetici olarak çalıştırmayı deneyin.", f"ÖBA Otomatik Oynatıcı | {'Kurulum' if mode == 'install' else 'Güncelleme Sistemi'}")
+        log.close()
+        exit(1)
 
     except Exception as exc:
         exc_tb = format_exc()
-        install_log.write(f"An unexpected error occured:\n{exc_tb}\n")
-        gui.show_error(0,f"Beklenmedik bir hata oluştu:\n\n{exc_tb}", "Player | Kurulum")
+        gui.show_error(f"Beklenmedik bir hata oluştu:\n\n{exc_tb}", f"ÖBA Otomatik Oynatıcı | {'Kurulum' if mode == 'install' else 'Güncelleme Sistemi'}")
+        log.close()
         exit(1)
 
     else:
-        install_log.write("Installation completed without any issues.")
+        log.write(f"{'Installation' if mode == 'install' else 'Update'} completed without any issues.")
+        log.close()
+
+def download_program_contents(mode):
+    global verify_ssl
+    
+    log.write("Getting app contents from Github...")
+    try:
+        return (0, server.get_program_contents(timeout=3,verify=verify_ssl))
+    except exceptions.SSLError:
+        log.write(f"An error occured due to SSL certificate authentication:\n{format_exc()}\n")
+        
+        ask_no_ssl_verify = gui.ask_and_show_error(f"SSL doğrulaması başarısız oldu. SSL doğrulamasını es geçip gene de devam etmek istiyor musunuz?\n(Bu yöntem güvenlik açıklarına sebep olacağından tavsiye edilmez.)",f"ÖBA Otomatik Oynatıcı | {'Kurulum' if mode == 'install' else 'Güncelleme Sistemi'}")        
+        if ask_no_ssl_verify:
+           verify_ssl = False
+           return download_program_contents(mode)
+               
+        else:
+            log.close()
+            return (1, None)
+            
+    except exceptions.ConnectTimeout:
+        log.write(f"Connection timed out:\n{format_exc()}\n")
+            
+        gui.show_error(f"Sunucuya gönderilen istek zaman aşımına uğradı.",f"ÖBA Otomatik Oynatıcı | {'Kurulum' if mode == 'install' else 'Güncelleme Sistemi'}")
+        log.close()
+        return (1, None)
+        
+    except exceptions.ConnectionError:
+        log.write(f"An error occured while getting app contents from Github:\n{format_exc()}\n")
+            
+        gui.show_error(f"Program internet olmadığından bilgisayara indirilemedi.",f"ÖBA Otomatik Oynatıcı | {'Kurulum' if mode == 'install' else 'Güncelleme Sistemi'}")
+        log.close()
+        return (1, None)
         
 
-def start_updater():
-    global VERIFY_SSL
-    updater_log = Log("Updater")
+def start_installer():
+    global verify_ssl, log
+    log.start()
+
+    is_error_occured, content_bytes = download_program_contents(mode="install")
+    if is_error_occured:
+        exit(1)
+
+
+    is_error_occured = install_program_contents(content_bytes,mode="install")
+    if is_error_occured:
+        exit(1)
+
+
+
     
-    updater_log.write("Getting version information from Github...")
+def start_updater():
+    global verify_ssl, log
+    log.start()
+
+    log.write(f"Reading registry value at: \"{FULL_KEY_PATH}\"")
+    local_version = registry.read_key(HKEY_CURRENT_USER,KEY_PATH,KEY_NAME)
+    log.write(f"Read registry value at: \"{FULL_KEY_PATH}\"")
+
+    
+    log.write("Getting version information from Github...")
     try:
-        local_version = registry.read_key(HKEY_CURRENT_USER,"SOFTWARE\\Player","version")
         main_version = server.get_version()
     except exceptions.SSLError:
-        updater_log.write(f"An error occured due to SSL certificate authentication:\n{format_exc()}\n")
+        log.write(f"An error occured due to SSL certificate authentication:\n{format_exc()}\n")
         
-        ask_no_ssl_verify = gui.ask_and_show_error("SSL doğrulaması başarısız oldu. SSL doğrumasını es geçip gene de devam etmek istiyor musunuz?\n(Bu yöntem güvenlik açıklarına sebep olacağından tavsiye edilmez.)","Player | Güncelleme Sistemi")        
+        ask_no_ssl_verify = gui.ask_and_show_error("SSL doğrulaması başarısız oldu. SSL doğrumasını es geçip gene de devam etmek istiyor musunuz?\n(Bu yöntem güvenlik açıklarına sebep olacağından tavsiye edilmez.)","ÖBA Otomatik Oynatıcı | Güncelleme Sistemi")        
         if ask_no_ssl_verify:
-            VERIFY_SSL = False
+            verify_ssl = False
             start_updater()
         else:
             pass
             
     except Exception:
-        updater_log.write(f"An error occured while getting app contents from Github:\n{format_exc()}\n")
+        log.write(f"An error occured while getting app contents from Github:\n{format_exc()}\n")
     
     else:
         if local_version == main_version:
-            updater_log.write("Application is already up to date.")
+            log.write("Application is already up to date.")
         
         elif local_version != main_version:
-            updater_log.write("Application isn't up to date.")
+            log.write("Application isn't up to date.")
 
-            updater_log.write("Getting app contents from Github...")
-            try:
-                content_bytes = server.get_program_contents(timeout=3,verify=VERIFY_SSL)
-                
-            except exceptions.ConnectTimeout:
-                updater_log.write(f"Connection timed out:\n{format_exc()}\n")
-            
-                gui.show_error("Sunucuya gönderilen istek zaman aşımına uğradı.","Player | Kurulum")
-                exit(1)
-            
-            except exceptions.ConnectionError:
-                updater_log.write(f"An error occured while getting app contents from Github:\n{format_exc()}\n")
-            
-                gui.show_error("Program internet olmadığından bilgisayara indirilemedi.","Player | Kurulum")
-                exit(1)
-            
-            
-            try:
-                if any(b"player_gui.exe.exe" in i for i in process_list()):
-                    kill("player_gui.exe.exe")
-                    updater_log.write("Killed 'player_gui.exe'")
-
-                        
-                updater_log.write(f"Creating temporary folder: \"{TEMP_PATH}\"")
-                file.create_dir(TEMP_PATH)
-
-                updater_log.write(f"Downloading app contents...")
-                file.write_byte(f"{TEMP_PATH}\\executable.zip",content_bytes)
-                updater_log.write(f"App contents have downloaded.")
-
-                updater_log.write("App folder is cleaning...")
-                file.remove_all(PROGRAM_PATH)
-                file.create_dir(PROGRAM_PATH)
-                updater_log.write("App folder been cleaned.")
-                         
-                updater_log.write("Extracting contents from temporary zip file...")
-                file.extract(f"{TEMP_PATH}\\executable.zip",PROGRAM_PATH)
-                updater_log.write("Contents have extracted from zip file.")
-
-                updater_log.write("Deleting temporary folder...")
-                file.remove_all(TEMP_PATH)
-                updater_log.write("Temporary folder been deleted.")
-                        
-                updater_log.write("Updating registry value at: \"HKEY_CURRENT_USER\\SOFTWARE\\Player\\version\"")
-                registry.create_key(HKEY_CURRENT_USER,"SOFTWARE\\Player","version",server.get_version())
-                updater_log.write("Updated registry value at: \"HKEY_CURRENT_USER\\SOFTWARE\\Player\\version\"")
-
-            except PermissionError as exc:
-                gui.show_error("Yetki hatası. Uygulamayı yönetici olarak çalıştırmayı deneyin.", "Player | Güncelleme Sistemi")
+            is_error_occured, content_bytes = download_program_contents(mode="update")
+            if is_error_occured:
                 exit(1)
 
-            except Exception as exc:
-                exc_tb = format_exc()
-                gui.show_error(f"Beklenmedik bir hata oluştu:\n\n{exc_tb}", "Player | Güncelleme Sistemi")
+         
+            is_error_occured = install_program_contents(content_bytes,mode="update")
+            if is_error_occured:
                 exit(1)
 
-            else:
-                updater_log.write("Update completed without any issues.")
-                        
 
 
 def main():
-    if not registry.check_key_exists(HKEY_CURRENT_USER,"SOFTWARE\\Player","version"):
+    global log
+    
+    if not registry.check_key_exists(HKEY_CURRENT_USER,KEY_PATH,KEY_NAME):
+        log = Log("Installer")
         start_installer()
     else:
+        log = Log("Updater")
         start_updater()
     
-    if not any(b"player_gui.exe" in i for i in process_list()):
-        start(f"{PROGRAM_PATH}\\player_gui.exe")
+    start(f"{PROGRAM_PATH}\\oba_gui.exe")
 
-    
+    log.close()
     exit(0)
 
 if __name__ == "__main__":
     main()
-       
+        
+
+    
+    
