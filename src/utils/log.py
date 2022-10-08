e76dc273ead1e1965b0638f675bab5f9c1d9ee98
MehmetKLl <110687128+MehmetKLl@users.noreply.github.com>
2022-10-08 21:23:02 +0300

    Update log.py

--- src/utils/log.py
+++ src/utils/log.py
@@ -6,15 +6,12 @@ from utils.process import start
 
 class Log:
     def __init__(self,filename):
-        self.filename = filename
-            
-
-    def start(self):
         if not path.exists(LOG_PATH):
             mkdir(LOG_PATH)
 
         self.filename = f"{self.filename} - {strftime('%d.%m.%Y %H.%M.%S')}.log"
-        self.file_io = open(f"{LOG_PATH}\\{self.filename}","w")
+        self.file_io = open(f"{LOG_PATH}\\{self.filename}","w") 
+            
             
     def write(self, text):
         self.file_io.write(f"[{strftime('%d.%m.%Y %H:%M:%S')}] {text}\n")
