84c8c4181a7d7d8d7def5e196e222bd0ac2c31dc
MehmetKLl <110687128+MehmetKLl@users.noreply.github.com>
2022-12-17 11:47:33 +0300

    Delete env_paths.py

--- src/utils/env_paths.py
+++
@@ -1,8 +0,0 @@
-from os import environ
-
-PROGRAM_PATH = f"{environ['APPDATA']}\\OBA Otomatik Oynatici"
-TEMP_PATH = f"{environ['TEMP']}\\Oba Otomatik Oynatici"
-LOG_PATH = f"{environ['TEMP']}\\OBA Log"
-KEY_PATH = "SOFTWARE\\OBA Otomatik Oynatici"
-FULL_KEY_PATH = "HKEY_CURRENT_USER\\SOFTWARE\\OBA Otomatik Oynatici\\version"
-KEY_NAME = "version"
