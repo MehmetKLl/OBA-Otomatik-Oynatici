42a8c3a87036d5351354143836874c928a510a27
MehmetKLl <tudkhahq@gmail.com>
2023-04-17 11:55:41 +0300

    v1.4.7 Update

--- src/autoplayer/main.py
+++ src/autoplayer/main.py
@@ -29,7 +29,7 @@ def scroll_and_start_new_video(scroll_delay=SCROLL_DELAY, video_check_delay=VIDE
         new_video_icon = videos_icons_screen.capture("img/new_video_icon.png")
         if new_video_icon:
             x, y, w, h = new_video_icon.get_pos()
-            start_new_video((x+5,y-5))
+            start_new_video((x+3,y-2))
             scroll_current_video(scroll_delay=scroll_delay)
             control_current_video(scroll_delay=scroll_delay, video_check_delay=video_check_delay)
             break
@@ -37,7 +37,7 @@ def scroll_and_start_new_video(scroll_delay=SCROLL_DELAY, video_check_delay=VIDE
         current_video_icon = videos_icons_screen.capture("img/current_icon.png")
         if current_video_icon:
             x, y, w, h = current_video_icon.get_pos()
-            start_new_video((x+5,y-5))
+            start_new_video((x+3,y-2))
             scroll_current_video(scroll_delay=scroll_delay)
             control_current_video(scroll_delay=scroll_delay, video_check_delay=video_check_delay)
             break
