36dbc6a5ffc4430067c7e6895b064b018e019d07
MehmetKLl <110687128+MehmetKLl@users.noreply.github.com>
2023-04-17 17:52:59 +0300

    v1.4.7 Update

--- src/autoplayer/handler.py
+++ src/autoplayer/handler.py
@@ -3,7 +3,7 @@ from win32con import SRCCOPY
 from win32gui import GetDesktopWindow, GetWindowDC, DeleteObject, ReleaseDC
 from win32ui import CreateDCFromHandle, CreateBitmap
 from numpy import uint8, frombuffer, where
-from cv2 import cvtColor, COLOR_BGRA2RGB, imread, matchTemplate, TM_CCOEFF_NORMED
+from cv2 import cvtColor, COLOR_BGRA2BGR, COLOR_BGR2HSV, imread, matchTemplate, TM_CCOEFF_NORMED, split
 from .classes import CapturedImage
 from utils.exceptions import ImageNotFoundException
 
@@ -27,7 +27,7 @@ class Screen:
         bmp_info = bitmap.GetInfo()
         bmp_bytes = bitmap.GetBitmapBits(True)
 
-        self.screen = cvtColor(frombuffer(bmp_bytes, dtype=uint8).reshape((self.height,self.width,4)), COLOR_BGRA2RGB)
+        self.screen_array = cvtColor(cvtColor(frombuffer(bmp_bytes, dtype=uint8).reshape((self.height,self.width,4)), COLOR_BGRA2BGR), COLOR_BGR2HSV)
         
         DeleteObject(bitmap.GetHandle())
         savedc.DeleteDC()
@@ -39,16 +39,13 @@ class Screen:
     def capture(self, image=None, threshold=0.99, mode="single", exceptions="silent"):
         if mode not in ["single","all"] or exceptions not in ["silent","raise"]:
             raise ValueError("Invalid arguments.")
-
         
-        self.template_image = cvtColor(imread(image), COLOR_BGRA2RGB)
-        self.template_height, self.template_width, _ = self.template_image.shape
+        self.template_image_array = cvtColor(cvtColor(imread(image), COLOR_BGRA2BGR), COLOR_BGR2HSV)
+        self.template_height, self.template_width, _ = self.template_image_array.shape
 
+        match_template_array = where(matchTemplate(self.screen_array, self.template_image_array, TM_CCOEFF_NORMED) >= threshold)
 
-        match_template_array = matchTemplate(self.screen, self.template_image, TM_CCOEFF_NORMED)
-
-        matched_sub_images_cord_array = where(match_template_array >= threshold)
-        x_array, y_array = matched_sub_images_cord_array[1], matched_sub_images_cord_array[0]
+        x_array, y_array = match_template_array[1], match_template_array[0]
 
 
         if x_array.size == 0 or y_array.size == 0:
@@ -58,9 +55,9 @@ class Screen:
             return None
 
         if mode == "single":
-            return CapturedImage(x_array[0], y_array[0], self.template_width, self.template_height, self.template_image)
+            return CapturedImage(x_array[0], y_array[0], self.template_width, self.template_height, self.template_image_array)
 
-        matched_sub_images_objects = [CapturedImage(x, y, self.template_width, self.template_height, self.template_image) for x, y in zip(x_array, y_array)] 
+        matched_sub_images_objects = [CapturedImage(x, y, self.template_width, self.template_height, self.template_image_array) for x, y in zip(x_array, y_array)] 
         return matched_sub_images_objects
             
 
